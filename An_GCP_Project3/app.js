9ed78b2dc99b3ae6668f649fb5cd52291458ff98
CaiYongAn <itriA60181@itri.org.tw>
2018-01-18 10:53:32 +0800

    s

--- An_GCP_Project3/app.js
+++ An_GCP_Project3/app.js
@@ -1,11 +1,11 @@
 var WebSocketServer = require('ws').Server;
-var appengine = require('appengine');
+// var appengine = require('appengine');
 var http = require('http');
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(appengine.middleware.base);
+// app.use(appengine.middleware.base);
 app.get('/_ah/health', function(req, res) {
   res.set('Content-Type', 'text/plain');
   res.status(200).send('ok');
