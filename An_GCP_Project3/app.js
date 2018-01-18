be5d37ccb259b0ff90539cee80b6c6e7bb7128a6
CaiYongAn <itriA60181@itri.org.tw>
2018-01-18 11:37:16 +0800

    a

--- An_GCP_Project3/app.js
+++ An_GCP_Project3/app.js
@@ -2,31 +2,30 @@ var WebSocketServer = require('ws').Server;
 // var appengine = require('appengine');
 var http = require('http');
 var express = require('express');
-var app = express();
+var app = express(); 
+
+
 
 app.use(express.static(__dirname + '/public'));
 // app.use(appengine.middleware.base);
-app.get('/_ah/health', function(req, res) {
-  res.set('Content-Type', 'text/plain');
-  res.status(200).send('ok');
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
 });
 
-app.get('/_ah/start', function(req, res) {
-  res.set('Content-Type', 'text/plain');
-  res.status(200).send('ok');
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
 
-/*app.get('/_ah/stop', function(req, res) {
-  res.set('Content-Type', 'text/plain');
-  res.status(200).send('ok');
-  process.exit();
-});*/
-
-var server = http.createServer(app);
-server.on('request', app);
-server.listen(80, function () {
-   console.log('Listening on http://localhost:80');
- });
+// var server = http.createServer(app);
+// server.on('request', app);
+// server.listen(80, function () {
+//    console.log('Listening on http://localhost:80');
+//  });
 
 //server.listen(8080, '0.0.0.0');
 
